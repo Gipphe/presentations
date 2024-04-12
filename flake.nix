5bb8eafaf0cf3e652eec064300fc9f0fa17583ba
Victor Nascimento Bakke <gipphe@gmail.com>
2024-04-12 10:35:39 +0000

    Add flake app and package

--- flake.nix
+++ flake.nix
@@ -6,8 +6,17 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        server = pkgs.writeShellScriptBin "marp" ''
+          ${pkgs.marp-cli}/bin/marp --server --watch .
+        '';
       in {
+        apps.default = {
+          type = "app";
+          program = "${server}/bin/marp";
+        };
+        packages = { inherit server; };
         devShell =
           pkgs.mkShell { buildInputs = with pkgs; [ nixfmt marp-cli docker ]; };
       });
